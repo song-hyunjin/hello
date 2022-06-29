ea9d2c5f7604a18e4642bafb18ac0a340246c770
Administrator <Administrator@SC-202106161522>
2022-06-29 10:13:09 +0900

    new 두번

--- 220629-01 git/src/Main.java
+++ 220629-01 git/src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
 	public static void main(String[] args) {
-		//헬로 월드 두번완성!
-		System.out.println("Hello World");
-		System.out.println("Hello World");
+		System.out.println("hello");
+		System.out.println("hello");
 	}
 }
